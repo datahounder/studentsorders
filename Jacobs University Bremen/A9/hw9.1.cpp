243797df0358d89ff26f7c39290eb2babfb257e4
datahounder <34120372+datahounder@users.noreply.github.com>
2025-04-14 01:43:55 +0500

    Add files via upload

---
+++ Jacobs University Bremen/A9/hw9.1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //created string, used cin to write it
+    //cout in the end to print it.
+    string country;
+    cout << "Write your origin country:" << endl;
+    cin>>country;
+    cout<<"Your country is: "<<country<<endl;
+    return 0;
+}
