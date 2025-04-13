2b0dd747bd85acab56525dc7760d4b8b17a1ebe8
datahounder <34120372+datahounder@users.noreply.github.com>
2025-04-14 01:52:00 +0500

    Add files via upload

---
+++ Jacobs University Bremen/a10/t2/main.cpp
@@ -0,0 +1,43 @@
+
+
+#include <iostream>
+#include <cstdlib>
+#include "Critter.h"
+
+using namespace std;
+
+//main function
+int main(int argc, char** argv)
+{
+	Critter c;
+
+	//decalred parameters to give
+	string name;
+	int hunger;
+	double height;
+	string color;
+
+	cout << endl << "Please enter data: " << endl;
+	cout << "Name: ";
+	// cin >> name; will not work if name contains
+	// spaces
+	getline(cin, name);
+	c.setName(name);
+
+	//set inputs
+	cout << "Hunger: ";
+	cin >> hunger;
+	c.setHunger(hunger);
+
+	cout<<"Color: ";
+	cin>>color;
+	c.setColor(color);
+
+	cout<<"Height: ";
+	cin>>height;
+	c.setHeight(height);
+
+	cout << "You have created:" << endl;
+	c.print();
+    return 0;
+}
